c8169960be90d5b10f649970ea24f38ef4535018
Rosemary-George <rosemarygeorge726@gmail.com>
2025-11-20 11:53:22 +0530

    feat: new feature ...

--- tests/functional-tests.spec.ts
+++ tests/functional-tests.spec.ts
@@ -7,4 +7,40 @@ test('Functional - valid user search shows profile', async ({ page }) => {
   await page.click('button');
 
   await expect(page.getByText('octocat')).toBeVisible();
+
+  //Valid user Search
+  test("Verify that Followers list loads after valid search", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    await page.fill('input[placeholder="Enter Github User Name"]', "nikolay_a00");
+    await page.click("text=Search");
+
+    await page.waitForSelector("text=Followers");
+
+    const followerItems = await page.locator(".followers-list > div").count();
+    expect(followerItems).toBeGreaterThan(0);
+  });
+  //Invalid User SEarch
+  test("Search for invalid GitHub user", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    await page.fill('input[placeholder=\"Enter Github User Name\"]', "invalid_user_12345_xx");
+    await page.click("text=Search");
+
+    const errorMessage = await page.locator(".error-message");
+    await expect(errorMessage).toBeVisible();
+  });
+
+  test("Verify request limit counter updates", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    const initialText = await page.textContent("text=Requests:");
+
+    await page.fill('input[placeholder=\"Enter Github User Name\"]', "nikolay_a00");
+    await page.click("text=Search");
+
+    const afterSearch = await page.textContent("text=Requests:");
+
+    expect(initialText).not.toBe(afterSearch);
+  });
 });
--- tests/ui-tests.spec.ts
+++ tests/ui-tests.spec.ts
@@ -11,3 +11,17 @@ test('UI - Page title is correct', async ({ page }) => {
   await page.goto('https://gh-users-search.netlify.app/');
   await expect(page).toHaveTitle(/Github User - Starter/i);
 });
+
+//Followers LIst UI Check
+test("Verify Followers list UI elements", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    await page.fill('input[placeholder=\"Enter Github User Name\"]', "nikolay_a00");
+    await page.click("text=Search");
+
+    await expect(page.locator("text=Followers")).toBeVisible();
+
+    const firstFollower = page.locator(".followers-list div").first();
+    await expect(firstFollower.locator("img")).toBeVisible();
+    await expect(firstFollower.locator("a")).toBeVisible();
+  });
